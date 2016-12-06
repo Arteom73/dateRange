c6ce31824c60ede12c37cfe92af29cef512045bb
a.ovchinnikov <artem9573@mail.ru>
2016-12-06 10:13:16 +0400

    prrr

--- dateRange.js
+++ dateRange.js
@@ -1,18 +1,19 @@
-var myPlugin = function () {
-    function myPlugin(params) {
-        if (!(this instanceof myPlugin)) {
-            return new myPlugin(params);
+var dateRange = function () {
+    function dateRange(params) {
+        if (!(this instanceof dateRange)) {
+            return new dateRange(params);
         }
         this.initialize.apply(this, arguments);
     }
-    myPlugin.prototype = {
+    dateRange.prototype = {
         initialize: function (options) {
             this.options = options;
+            this.setDefaultOptions();
 
             if(!this.options.from) {return false;}
             this.fromElement = document.getElementById(this.options.from) ;
 
-            if(!!this.options.from) {
+            if(!!this.options.to) {
                 this.toElement = document.getElementById(this.options.to) ;
             }
 
@@ -20,14 +21,19 @@ var myPlugin = function () {
         },
         handlers: function() {
             var _this = this;
+
+            if(!this.options.from) {return false;}
             this.fromElement.onfocus = function() {
-                _this.showCalendar('from');
                 _this.fromEvent = _this.fromElement;
+                _this.showCalendar('from');
             };
-            this.toElement.onfocus = function() {
-                _this.showCalendar('to');
-                _this.fromEvent = _this.toElement;
-            };
+
+            if(!!this.options.to) {
+                this.toElement.onfocus = function() {
+                    _this.fromEvent = _this.toElement;
+                    _this.showCalendar('to');
+                };
+            }
 
         },
         showCalendar: function (type, year, month) {
@@ -37,7 +43,15 @@ var myPlugin = function () {
             }
             this.calendar = document.createElement('div');
             this.calendar.id = this.options.id;
-            this.calendar.style.width = this.options.width;
+            if(this.options.width == '100%') {
+                this.calendar.style.width = this.fromEvent.offsetWidth;
+            } else {
+                this.calendar.style.width = this.options.width;
+            }
+            this.calendar.style.position = 'absolute';
+            this.calendar.style.left = this.fromEvent.getBoundingClientRect().left + 'px';
+            this.calendar.style.top = this.fromEvent.offsetHeight + this.fromEvent.getBoundingClientRect().top + 'px';
+
             var d;
 
             if(year && month) {
@@ -56,11 +70,11 @@ var myPlugin = function () {
                 return day - 1;
             };
 
-            _tmp += '<table style="width:100%; text-align:center">';
+            _tmp += '<table>';
             _tmp += '<tr><td data-event="change-month" data-year="' + (d.getMonth() - 1 < 0 ? d.getFullYear() - 1 : d.getFullYear()) + '" data-month="' + (d.getMonth() - 1 < 0 ? 11 : d.getMonth() - 1) + '"><</td><td>'+this.options.monthLabel[d.getMonth()]+' '+d.getFullYear()+'</td><td data-event="change-month" data-year="' + (d.getMonth() + 1 > 11 ? d.getFullYear() + 1 : d.getFullYear()) + '" data-month="' + (d.getMonth() + 1 > 11 ? 0 : d.getMonth() + 1) + '">></td></tr>';
             _tmp += '</table>';
 
-            _tmp += '<table style="width:100%">';
+            _tmp += '<table>';
             _tmp += '<tr><th>'+this.options.dayLabel[0]+'</th><th>'+this.options.dayLabel[1]+'</th><th>'+this.options.dayLabel[2]+'</th><th>'+this.options.dayLabel[3]+'</th><th>'+this.options.dayLabel[4]+'</th><th>'+this.options.dayLabel[5]+'</th><th>'+this.options.dayLabel[6]+'</th></tr><tr>';
 
             for (var i = 0; i < getDay(d); i++) {
@@ -68,7 +82,23 @@ var myPlugin = function () {
             }
 
             while (d.getMonth() == mon) {
-                _tmp += '<td data-event="select-day" data-year="' + d.getFullYear() + '" data-month="' + d.getMonth() + '" data-day="' + d.getDate() + '">' + d.getDate() + '</td>';
+
+                var dayDate = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+                if(type == 'from' && _this.toDate) {
+                    if(dayDate.getTime() > _this.toDate.getTime()) {
+                        _tmp += '<td class="disabled">' + d.getDate() + '</td>';
+                    } else {
+                        _tmp += '<td data-event="select-day" data-year="' + d.getFullYear() + '" data-month="' + d.getMonth() + '" data-day="' + d.getDate() + '">' + d.getDate() + '</td>';
+                    }
+                } else if(type == 'to' && _this.fromDate) {
+                    if(dayDate.getTime() < _this.fromDate.getTime()) {
+                        _tmp += '<td class="disabled">' + d.getDate() + '</td>';
+                    } else {
+                        _tmp += '<td data-event="select-day" data-year="' + d.getFullYear() + '" data-month="' + d.getMonth() + '" data-day="' + d.getDate() + '">' + d.getDate() + '</td>';
+                    }
+                } else {
+                    _tmp += '<td data-event="select-day" data-year="' + d.getFullYear() + '" data-month="' + d.getMonth() + '" data-day="' + d.getDate() + '">' + d.getDate() + '</td>';
+                }
 
                 if (getDay(d) % 7 == 6) {
                     _tmp += '</tr><tr>';
@@ -98,9 +128,18 @@ var myPlugin = function () {
                         case 'select-day':
                             _this.fromEvent.value = this.getAttribute('data-day') + '.' + this.getAttribute('data-month') + '.' + this.getAttribute('data-year');
                             if(type == 'from') {
-                                _this.fromDate = 'asd';
+                                _this.fromDate = new Date(this.getAttribute('data-year'), this.getAttribute('data-month'), this.getAttribute('data-day'));
+                                _this.hideCalendar();
+                                if(!!_this.options.to && !_this.toDate) {
+                                    _this.toElement.focus();
+                                }
+                            } else {
+                                _this.toDate = new Date(this.getAttribute('data-year'), this.getAttribute('data-month'), this.getAttribute('data-day'));
+                                _this.hideCalendar();
+                                if(!_this.fromDate) {
+                                    _this.fromElement.focus();
+                                }
                             }
-                            _this.hideCalendar();
                         break;
                         case 'change-month':
                             _this.hideCalendar();
@@ -114,16 +153,28 @@ var myPlugin = function () {
         hideCalendar: function () {
             this.calendar.outerHTML = "";
             delete this.calendar;
+        },
+        setDefaultOptions: function() {
+            if(!this.options.width) {
+                this.options.width = '100%';
+            }
+            if(!this.options.id) {
+                this.options.id = 'dateRange';
+            }
+            if(!this.options.monthLabel) {
+                this.options.monthLabel = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            }
+            if(!this.options.dayLabel) {
+                this.options.dayLabel = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+            }
         }
     };
-    return myPlugin;
+    return dateRange;
 }();
 
-myPlugin({
+dateRange({
     from: 'dateFrom',
     to: 'dateTo',
-    width: '200px',
-    id: 'dateRange',
     monthLabel: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
     dayLabel: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
 });
